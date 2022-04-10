cff07666402ba5df93e1f1dcf9600f18e3c1febe
Jason Wang <jasonherngwang@gmail.com>
2022-04-10 07:15:36 -0700

    Completed http server exercises.

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -1,14 +1,39 @@
-#!/bin/bash
+#!/usr/local/bin/bash
+echo "${BASH_VERSION}"
 
 function server () {
   while true
   do
-    message=read
+    message_arr=()                       # Array
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]          # Final line has length 1.
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    echo $method
+    echo $path
+    # read method path version
+    if [[ $method = 'GET' ]]             # = is for string equality
+    then
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text-html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
+    fi
   done
 }
 
-# coprocess: Enable server to run alongside nc
 coproc SERVER_PROCESS { server; }
 
-# Redirect nc's input and output to server.
-nc -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+nc -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
